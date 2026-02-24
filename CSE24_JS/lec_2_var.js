2131414cac65b3525bd835d81b5c14642875e6c6
Sahil yadav <sahilyadav902657@gmail.com>
2026-02-24 15:50:42 +0530

    lec-2

---
+++ CSE24_JS/lec_2_var.js
@@ -0,0 +1,13 @@
+
+var num = 10;
+console.log(num);
+console.log(typeof num);
+num = false;
+console.log(num);
+console.log(typeof num);
+num = "Hello";
+console.log(num);
+console.log(typeof num);
+num = undefined;
+console.log(num);
+console.log(typeof num);
---
+++ CSE24_JS/script.js
@@ -0,0 +1,11 @@
+console.log("hello world")
+document.write("hello world");
+
+
+
+<script type ="text/javascript">
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=flase;
+</script>
